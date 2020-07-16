4f200bc2675cd5bbd4b01c8eb3b7fe1155e5eb39
Marcus Desai <marcus.desai@gmail.com>
2020-07-16 11:57:48 +0100

    refactor collapse functionality into struct & methods

---
+++ src/collapse.rs
@@ -0,0 +1,84 @@
+use std::collections::{HashSet, BinaryHeap, HashMap};
+use std::ops::Index;
+use rand::prelude::*;
+use crate::observe::Observe;
+use crate::propagate::Propagate;
+use crate::graph::{Rules, Graph, VertexIndex, VertexLabel, Frequencies, Labels};
+use crate::utils::{hash_set, hash_map};
+
+struct Collapse<'a> {
+    heap: BinaryHeap<Observe>,
+    gen_observe: HashSet<VertexIndex>,
+    observed: HashSet<VertexIndex>,
+    propagations: Vec<Propagate>,
+    rng: &'a mut StdRng,
+    rules: &'a Rules,
+    frequencies: &'a Frequencies,
+    all_labels: &'a Labels,
+    out_graph: Graph,
+}
+
+impl Collapse<'_> {
+    pub fn new<'a>(rng: &'a mut StdRng,
+                   rules: &'a Rules,
+                   frequencies: &'a Frequencies,
+                   all_labels: &'a Labels,
+                   out_graph: Graph) -> Collapse<'a> {
+        let mut heap: BinaryHeap<Observe> = BinaryHeap::new();
+        let mut gen_observe: HashSet<VertexIndex> = HashSet::new();
+        let mut observed: HashSet<VertexIndex> = HashSet::new();
+        let mut propagations: Vec<Propagate> = Vec::new();
+
+        for (_index, labels) in out_graph.vertices.iter().enumerate() {
+            let from_index = _index as i32;
+            if labels.is_subset(all_labels) && labels != all_labels { // <-- labels is proper subset of all_labels
+                out_graph.connections(&from_index).iter().for_each(|(to_index, direction)| {
+                    propagations.push(Propagate::new(from_index, *to_index, *direction))
+                });
+                if labels.len() == 1 { // <-- labels is singleton set
+                    observed.insert(from_index);
+                    continue;
+                }
+            }
+            heap.push(Observe::new_fuzz(rng, &from_index, labels, frequencies))
+        }
+
+        Collapse {
+            heap,
+            gen_observe,
+            observed,
+            propagations,
+            rng,
+            rules,
+            frequencies,
+            all_labels,
+            out_graph
+        }
+    }
+
+    pub fn exec(&mut self) -> Option<Graph> {
+        let heap = &mut self.heap;
+        let observed = &mut self.observed;
+        let out_graph = &mut self.out_graph;
+        let gen_observe = &mut self.gen_observe;
+        let propagations = &mut self.propagations;
+
+        let frequencies = &self.frequencies;
+        let vertices = &out_graph.vertices;
+
+        loop {
+            if observed.len() == vertices.len() || heap.is_empty() {
+                return Some(std::mem::replace(out_graph, Graph::new(Vec::new(), HashMap::new())));
+            }
+            if propagations.is_empty() {
+                gen_observe.drain().for_each(|index| {  // algo: 4.2
+                    let labels = vertices.index(index as usize);
+                    heap.push(Observe::new(&index, labels, frequencies))
+                });
+                //heap.pop().unwrap()
+            } else {
+                // do propagate
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,8 @@
+mod collapse;
 mod observe;
 mod propagate;
 mod utils;
 mod graph;
-
-use std::collections::{HashSet, BinaryHeap, HashMap};
-use rand::prelude::*;
-use crate::observe::Observe;
-use crate::propagate::Propagate;
-use crate::graph::{Rules, Graph, VertexIndex, VertexLabel, Frequencies, Labels};
-use crate::utils::{hash_set, hash_map};
 
 /*
 TODO:
@@ -35,54 +29,4 @@ fn main() {
     //     output_graph_vertices,
     //     output_graph_edges
     // );
-}
-
-fn collapse_algorithm(rng: &mut StdRng, rules: &Rules, frequencies: &Frequencies, all_labels: &Labels, out_graph: Graph) -> Option<Graph> {
-    let mut heap: BinaryHeap<Observe> = BinaryHeap::new();
-    let mut gen_observe: HashSet<VertexIndex> = HashSet::new();
-    let mut observed: HashSet<VertexIndex> = HashSet::new();
-    let mut propagations: Vec<Propagate> = Vec::new();
-
-    initialize(rng, frequencies, all_labels, &out_graph, &mut propagations, &mut observed, &mut heap);
-
-    loop {
-        if observed.len() == out_graph.vertices.len() || heap.is_empty() { return Some(out_graph) }
-        if propagations.is_empty() {
-            gen_observe.drain().for_each(|index| {  // algo: 4.2
-                let labels = out_graph.vertices.get(index as usize).unwrap();
-                heap.push(Observe::new(&index, labels, frequencies))
-            });
-
-            //heap.pop().unwrap()
-        } else {
-            // do propagate
-        }
-    }
-
-    None
-}
-
-fn initialize(
-    rng: &mut StdRng, 
-    frequencies: &Frequencies, 
-    all_labels: &Labels, 
-    out_graph: &Graph, 
-    propagations: &mut Vec<Propagate>,
-    observed: &mut HashSet<VertexIndex>,
-    heap: &mut BinaryHeap<Observe>
-) {
-    for (_index, labels) in out_graph.vertices.iter().enumerate() {
-        let from_index = _index as i32;
-        if labels.is_subset(all_labels) && labels != all_labels { // <-- labels is proper subset of all_labels
-            out_graph.connections(&from_index).iter().for_each(|(to_index, direction)| {
-                propagations.push(Propagate::new(from_index, *to_index, *direction))
-            });
-
-            if labels.len() == 1 { // <-- labels is singleton set
-                observed.insert(from_index);
-                continue
-            }
-        }
-        heap.push(Observe::new_fuzz(rng, &from_index, labels, frequencies))
-    }
 }
