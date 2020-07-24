90fecc6b422c4b08ccf1a318bf81112c5bda8df9
dpwdec <declankolakowski@gmail.com>
2020-07-24 11:13:08 +0100

    Refactor utils to use where syntax for clearer method signature

--- src/utils.rs
+++ src/utils.rs
@@ -2,10 +2,15 @@ use std::collections::{HashMap, HashSet};
 use std::hash::Hash;
 use std::iter::FromIterator;
 
-pub fn hash_set<T: Hash + Eq + Clone>(data: &[T]) -> HashSet<T> {
+pub fn hash_set<T>(data: &[T]) -> HashSet<T> 
+    where T: Hash + Eq + Clone
+{
     HashSet::from_iter(data.iter().cloned())
 }
 
-pub fn hash_map<K: Hash + Eq + Clone, V: Clone>(data: &[(K, V)]) -> HashMap<K, V> {
+pub fn hash_map<K, V>(data: &[(K, V)]) -> HashMap<K, V> 
+    where K: Hash + Eq + Clone,
+          V: Clone
+{
     HashMap::from_iter(data.iter().cloned())
 }
