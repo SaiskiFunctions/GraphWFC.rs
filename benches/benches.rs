b4dc36cc39e6b7f4863ba20c3bb2ae2d5fd377b3
Marcus Desai <marcus.desai@gmail.com>
2020-08-01 18:49:20 +0100

    implement own ver. of cached calculate_entropy

--- benches/benches.rs
+++ benches/benches.rs
@@ -20,14 +20,13 @@ mod collapse {
             let output_edges = make_nsew_grid_edges(out_width, out_depth);
             let output_graph = Graph::new(output_vertices, output_edges);
 
-            bench.iter(|| { collapse(&input_graph, output_graph.clone(), Some(134522), None) })
+            bench.iter(|| { collapse(&input_graph, output_graph.clone(), None, None) })
         }
     }
 }
 
-mod nalgebra_tests {
+mod nalgebra_test {
     use bencher::Bencher;
-    use wfc_rust::graph::graph::Graph;
     use nalgebra::{inf, DVector};
 
 
@@ -54,11 +53,143 @@ mod nalgebra_tests {
     }
 }
 
+mod entropy_cache {
+    use bencher::Bencher;
+    use wfc_rust::wfc::observe::*;
+    use wfc_rust::utils::{hash_set, hash_map};
+    use nalgebra::DVector;
+
+    pub fn uncached_16(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
+        let test_frequencies = hash_map(&[
+            (1, 200), (2, 100), (3, 33), (4, 28), (5, 99), (6, 11), (7, 76), (8, 43),
+            (9, 200), (10, 100), (11, 33), (12, 28), (13, 99), (14, 11), (15, 76), (16, 43)
+        ]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_8(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2, 3, 4, 5, 6, 7, 8]);
+        let test_frequencies = hash_map(&[(1, 200), (2, 100), (3, 33), (4, 28), (5, 99), (6, 11), (7, 76), (8, 43)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_7(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2, 3, 4, 5, 6, 7]);
+        let test_frequencies = hash_map(&[(1, 200), (2, 100), (3, 33), (4, 28), (5, 99), (6, 11), (7, 76)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_6(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2, 3, 4, 5, 6]);
+        let test_frequencies = hash_map(&[(1, 200), (2, 100), (3, 33), (4, 28), (5, 99), (6, 11)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_5(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2, 3, 4, 5]);
+        let test_frequencies = hash_map(&[(1, 200), (2, 100), (3, 33), (4, 28), (5, 99)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_4(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2, 3, 4]);
+        let test_frequencies = hash_map(&[(1, 200), (2, 100), (3, 33), (4, 28)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_3(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2, 3]);
+        let test_frequencies = hash_map(&[(1, 200), (2, 100), (3, 33)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_2(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2]);
+        let test_frequencies = hash_map(&[(1, 200), (2, 100)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_1(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1]);
+        let test_frequencies = hash_map(&[(1, 200)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn uncached_zeros_8(bench: &mut Bencher) {
+        let test_labels = hash_set(&[1, 2, 3, 4, 5]);
+        let test_frequencies = hash_map(&[(1, 200), (2, 0), (3, 0), (4, 0), (5, 0)]);
+        bench.iter(|| {
+            calculate_entropy(&test_labels, &test_frequencies)
+        })
+    }
+
+    pub fn cached_ent2_8(bench: &mut Bencher) {
+        let a: DVector<u32> = DVector::from_row_slice(&[200, 100, 33, 28, 99, 11, 76, 43]);
+        bench.iter(|| {
+            calculate_entropy2(a.clone())
+        })
+    }
+
+    pub fn cached_ent3_8(bench: &mut Bencher) {
+        let a: &DVector<u32> = &DVector::from_row_slice(&[200, 100, 33, 28, 99, 11, 76, 43]);
+        bench.iter(|| {
+            calculate_entropy3(a)
+        })
+    }
+
+    pub fn cached_ent3_16(bench: &mut Bencher) {
+        let a: &DVector<u32> = &DVector::from_row_slice(&[200, 100, 33, 28, 99, 11, 76, 43, 200, 100, 33, 28, 99, 11, 76, 43]);
+        bench.iter(|| {
+            calculate_entropy3(a)
+        })
+    }
+
+    pub fn cached_ent3_zeroes_8(bench: &mut Bencher) {
+        let a: &DVector<u32> = &DVector::from_row_slice(&[200, 0, 0, 0, 0, 0, 0, 0]);
+        bench.iter(|| {
+            calculate_entropy3(a)
+        })
+    }
+}
+
 
 benchmark_group!(
     benches,
-    collapse::bench_collapse,
-    nalgebra_tests::vector_inf,
-    nalgebra_tests::vector_wise_mul
+    // collapse::bench_collapse,
+    // nalgebra::vector_inf,
+    // nalgebra_test::vector_wise_mul,
+    entropy_cache::uncached_16,
+    entropy_cache::uncached_8,
+    entropy_cache::uncached_7,
+    entropy_cache::uncached_6,
+    entropy_cache::uncached_5,
+    entropy_cache::uncached_4,
+    entropy_cache::uncached_3,
+    entropy_cache::uncached_2,
+    entropy_cache::uncached_1,
+    entropy_cache::cached_ent2_8,
+    entropy_cache::cached_ent3_8,
+    entropy_cache::cached_ent3_16,
+    entropy_cache::uncached_zeros_8,
+    entropy_cache::cached_ent3_zeroes_8
 );
 benchmark_main!(benches);
--- src/wfc/mod.rs
+++ src/wfc/mod.rs
@@ -1,3 +1,3 @@
 pub mod collapse;
-mod observe;
+pub mod observe;
 mod propagate;
