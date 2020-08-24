c633b8db8f64cc73d234be672b2e2fa79d4bf9ad
Dec Kolakowski <declankolakowski@gmail.com>
2020-08-24 18:55:17 +0100

    Add matrix rotation exploration

--- src/io/image_olm_parser.rs
+++ src/io/image_olm_parser.rs
@@ -7,35 +7,38 @@ static RGB_CHANNELS: u8 = 3;
 
 pub fn parse() {
     let img = image::open("resources/test/City.png").unwrap();
-    // 
-    // chunk {
-    //     vec![4]
-    // }
 }
 
-// fn chunk_image(image: &DynamicImage) -> HashSet<Vec<u8>> {
-//     let chunk_size = 2;
-//     let sub_chunk_size = 1; // pixels per sub chunk
 
-//     let (height, width) = image.dimensions();
 
-//     let mut chunk_set: HashSet<Vec<u8>> = HashSet::new();
+fn chunk_image(image: &DynamicImage) -> HashSet<Vec<u8>> {
+    let chunk_size = 2;
+    let sub_chunk_size = 1; // pixels per sub chunk
 
-//     (0..height - (chunk_size - 1)).for_each(|y| {
-//         (0..width - (chunk_size - 1)).for_each(|x| {
-//             // get vec of pixel channels
-//             chunk = DMatrix.for_row_vector(chunk_size, chunk_size, image.crop_imm(x, y, chunk_size, chunk_size).to_rgb().into_vec().chunks(RGB_CHANNELS));
+    let (height, width) = image.dimensions();
 
-//             if !chunk_set.contains(chunk) { 
-//                 insert(chunk);
-//             };
+    let mut chunk_set: HashSet<Vec<u8>> = HashSet::new();
 
-//             // for each cardinality
-//         });
-//     });
+    (0..height - (chunk_size - 1)).for_each(|y| {
+        (0..width - (chunk_size - 1)).for_each(|x| {
+            // get vec of pixel channels
+            chunk = DMatrix.for_row_vector(chunk_size, chunk_size, image.crop_imm(x, y, chunk_size, chunk_size).to_rgb().into_vec().chunks(RGB_CHANNELS));
 
-//     chunk_set
-// }
+            if !chunk_set.contains(chunk) { 
+                chunk_set.insert(chunk);
+            };
+
+            // for each cardinality
+
+            // rotate 90
+            // insert it
+            // rotate 90
+            // insert it
+        });
+    });
+
+    chunk_set
+}
 
 pub fn is_sym(matrix: &DMatrix<i32>) -> bool {
     let transposed_matrix = matrix.transpose();
@@ -47,6 +50,23 @@ pub fn is_sym(matrix: &DMatrix<i32>) -> bool {
 mod tests {
 
     use super::*;
+
+    // we need to be more aware of when trait implementations imply things about syntactical constructs
+
+    #[test]
+    fn test_move_data() {
+        let side = 4;
+        let basic_matrix = DMatrix::from_column_slice(side, side, &vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16]);
+        let mut target_matrix = DMatrix::<u32>::zeros(side, side);
+
+        (0..side).for_each(|i| {
+            (0..side).for_each(|j| {
+                target_matrix[(j, (side - 1) - i)] = basic_matrix[(i, j)]
+            });
+        });
+    }
+
+
 
     #[test]
     fn test_image_loading() {
@@ -93,15 +113,15 @@ mod tests {
     fn test_symmetry() {
         let asym_matrix = DMatrix::from_row_slice(2, 3, &vec![2, 1, 2, 0, 2, 1]);
         let trans_asym_matrix = asym_matrix.transpose();
-        asym_matrix.row(0).into_iter().for_each(|r| { println!("{:?}", r)});
-        trans_asym_matrix.row(0).into_iter().for_each(|r| { println!("{:?}", r)});
+        // asym_matrix.row(0).into_iter().for_each(|r| { println!("{:?}", r)});
+        // trans_asym_matrix.row(0).into_iter().for_each(|r| { println!("{:?}", r)});
         //println!("{}", asym_matrix.row(0).iter());
 
         let sym_matrix = DMatrix::from_row_slice(2, 2, &vec![2, 1, 2, 0]);
         // let trans_sym_matrix = sym_matrix.transpose();
         // println!("{:?}", sym_matrix == trans_sym_matrix);
-        assert!(!is_sym(&asym_matrix));
-        assert!(is_sym(&sym_matrix));
+        // assert!(!is_sym(&asym_matrix));
+        // assert!(is_sym(&sym_matrix));
     }
     // 2 1
     // 2 0   2 2 2
@@ -116,6 +136,16 @@ mod tests {
     // 2 3
     // 4 5
 
+    // 
+
     // 3 5
     // 2 4
+
+    // 1 1 1
+    // 0 0 0
+    // 1 1 1
+
+    // 0 1 0
+    // 1 1 1
+    // 0 1 0
 }
