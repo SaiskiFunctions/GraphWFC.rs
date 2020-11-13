e12a8922cdb103ab3e2c9ff6c0e85b60d9b0b12a
Dec Kolakowski <declankolakowski@gmail.com>
2020-11-13 17:11:39 +0000

    Add submatrix functions

--- src/io/image_olm_parser.rs
+++ src/io/image_olm_parser.rs
@@ -5,6 +5,7 @@ use itertools::Itertools;
 use nalgebra::geometry::Rotation2;
 use nalgebra::{DMatrix, Matrix2};
 use std::collections::HashSet;
+use num_traits::{Zero, One};
 
 // Matrix and image data is in COLUMN MAJOR so:
 // [1, 2, 3, 4] is equivalent to:
@@ -111,58 +112,194 @@ fn chunk_image(
         })
 }
 
-// image overlaps? fn
-fn overlaps(
-    target: &DMatrix<u32>,
-    overlap: &DMatrix<u32>,
-    target_position: (u32, u32),
-    overlap_position: (u32, u32),
-    chunk_size: u32,
-) -> Bool {
-    // 2D iteration through a chunk
-    (0..chunk_size)
-        .cartesian_product(0..chunk_size)
-        // assume overlap is possible
-        .fold(true, |acc, (y, x)| {
-            // if in a non negative space
-            if not_negative_coord(y - overlap_position.0, x - overlap_position.1) {
-                // compare the value at the current REAL SPACE position of target
-                // to the current position in overlap
-                if target[(y - overlap_position.0, x - overlap_position.1)] != overlap[(y, x)] {
-                    acc = false; // if they don't overlap assumption is falsified
-                }
-            }
+// ================================================================================================
+// ================================================================================================
+// ================================================================================================
+
+
+trait SubMatrix {
+    fn crop_left(self, offset: usize) -> DMatrix<u32>;
+    fn crop_right(self, offset: usize) -> DMatrix<u32>;
+    fn crop_top(self, offset: usize) -> DMatrix<u32>;
+    fn crop_bottom(self, offset: usize) -> DMatrix<u32>;
+    fn sub_matrix(&self, position: (usize, usize), size: (usize, usize)) -> DMatrix<u32>;
+}
+
+impl SubMatrix for DMatrix<u32> {
+    fn crop_left(self, offset: usize) -> DMatrix<u32> {
+        self.remove_columns(0, offset)
+    }
+
+    fn crop_right(self, offset: usize) -> DMatrix<u32> {
+        let cols = self.ncols();
+        if offset >= cols { return self }
+        self.remove_columns(offset, cols - offset)
+    }
+
+    fn crop_top(self, offset: usize) -> DMatrix<u32> {
+        self.remove_rows(0, offset)
+    }
+
+    fn crop_bottom(self, offset: usize) -> DMatrix<u32> {
+        let rows = self.nrows();
+        if offset >= rows { return self }
+        self.remove_rows(1, rows - offset)
+    }
+
+    fn sub_matrix(&self, position: (usize, usize), size: (usize, usize)) -> DMatrix<u32> {
+        self
+            .clone()
+            .crop_left(position.0)
+            .crop_right(size.0)
+            .crop_top(position.1)
+            .crop_bottom(size.1)
+    }
+}
+
+/*
+    1. Convert chunk_set to a map with indexes
+        Each chunk needs to be assigned a label (this is the same as a VertexLabel)
+
+    2. Compute basic overlaps of chunks in the 8 cardinal and inter-cardinal directions:
+        a. Generate sub chunk positions
+        b. create an accessor to get Matrix chunks
+    3. Compute overlaps in a list of overlap structs (N-1 number of overlapping steps) -> but only 1 overlapping map
+    3. Use the overlap struct to create a connections struct
+ */
+
+// for the case of N=3 we still just connect via cardinals
+
+// This can just be a graph
+// type ChunkIndex = i32;
+// struct Chunk {
+//     pub pixels: DMatrix<u32>,
+//     pub connections:
+// }
+
+// a function that returns a list of 4D tuples that contain sub chunk positions
+// we don't care about the case where the overlap is the entire chunk
+type Position = (i32, i32);
+type Size = (i32, i32);
+type Direction = i32;
+// 0 => NW, 1 => NE, 2 => SW, 3 => SE
+// 4 => W, 5 => E, 6 => N, 7 => S
+// rewrite this shit
+fn sub_chunk_positions(chunk: &DMatrix<u32>, chunk_size: u32) -> Vec<(Position, Size, Direction)> {
+    //(y, x) (height, width) (dir)
+    vec![
+        ((0, 0), (1, 1), 0),
+        ((0, 1), (1, 1), 1),
+        ((1, 0), (1, 1), 2),
+        ((1, 1), (1, 1), 3),
+        ((0, 0), (2, 1), 4),
+        ((0, 1), (2, 1), 5),
+        ((0, 0), (1, 2), 6),
+        ((1, 0), (1, 2), 7)
+    ]
+}
+
+fn set_to_map<T>(set: HashSet<T>) -> HashMap<u32, T> {
+    set
+        .into_iter()
+        .enumerate()
+        .fold(HashMap::<u32, T>::new(), |mut acc, (k, v)| {
+            acc.insert(k as u32, v);
             acc
         })
-    // translate overlap matrix into target matrix space
-    // reference overlaps by same index coordiante system
-    // compare the values
-
-    // calculate translation values
-    // translation: (i32, i32)
-
-    // 0, 1 + 0, 0 2-0 = 2 2-1 = 1
-    // 1, 0, 1, 1
-    // overlap.get_pixel(
 }
+// what structure does this actually return?
+// for each chunk it should return the chunk and a list of its connections (and the direction?)
+// so find all the intermediate connections -> do we need to know the direction for that?
+
+fn overlaps_2(chunk_set: HashSet<DMatrix<u32>>, chunk_size: u32) { //-> Vec<(DMatrix<u32>, Vec<DMatrix<u32>>)>
+    // give each chunk in the set an Id
+    // let chunk_list = set_to_map::<DMatrix<u32>>(chunk_set);
+    let chunk_map = set_to_map(chunk_set);
+    // overlapping based on sub_chunks
+    // chunk_set
+    //     .iter()
+    //     .for_each(|chunk| {
+    //         let scp = sub_chunk_positions(chunk, chunk_size);
+    //         chunk_set
+    //             .iter()
+    //             .filter(|x| x != chunk)
+    //             .map(|x| x.rotate_90().rotate_90())
+    //             .for_each(|overlap| {
+    //                 chunk.overlaps?(overlap, scp);
+    //                 //
+    //             })
+    //     });
+
+    // return x;
+}
+
+// image overlaps? fn
+// fn overlaps(
+//     target: &DMatrix<u32>,
+//     overlap: &DMatrix<u32>,
+//     target_position: (u32, u32),
+//     overlap_position: (u32, u32),
+//     chunk_size: u32,
+// ) -> Bool {
+//     // 2D iteration through a chunk
+//     (0..chunk_size)
+//         .cartesian_product(0..chunk_size)
+//         // assume overlap is possible
+//         .fold(true, |acc, (y, x)| {
+//             // if in a non negative space
+//             if not_negative_coord(y - overlap_position.0, x - overlap_position.1) {
+//                 // compare the value at the current REAL SPACE position of target
+//                 // to the current position in overlap
+//                 if target[(y - overlap_position.0, x - overlap_position.1)] != overlap[(y, x)] {
+//                     acc = false; // if they don't overlap assumption is falsified
+//                 }
+//             }
+//             acc
+//         })
+//     // translate overlap matrix into target matrix space
+//     // reference overlaps by same index coordiante system
+//     // compare the values
+//
+//     // calculate translation values
+//     // translation: (i32, i32)
+//
+//     // 0, 1 + 0, 0 2-0 = 2 2-1 = 1
+//     // 1, 0, 1, 1
+//     // overlap.get_pixel(
+// }
 
 // naive the connections
 
 // prunes connections
+// PartialOrd --> uses >= and <=
 
-fn not_negative(x: u32) -> bool {
-    return x > -1
-}
-
-fn not_negative_t<T>(x: T) -> bool
-    where T: Ord
+fn positive<T>(x: T) -> bool
+    where T: Zero + PartialOrd
 {
-    x > -1
+    return x >= Zero::zero()
 }
 
-fn not_negative_coord(x: u32, y: u32) -> bool {
-    return not_negative(x) && not_negative(y)
-}
+// fn significant<T>(x: T) -> bool
+//     where T: One + PartialOrd
+// {
+//     return x >= One::one()
+// }
+//
+// fn significant_2<T>(x: T) -> bool
+//     where T: Zero + Ord
+// {
+//     return x > Zero::zero()
+// }
+//
+// fn not_negative_t<T>(x: T) -> bool
+//     where T: Ord
+// {
+//     x > -1
+// }
+//
+// fn not_negative_coord(x: u32, y: u32) -> bool {
+//     return not_negative(x) && not_negative(y)
+// }
 
 #[cfg(test)]
 mod tests {
@@ -170,6 +307,31 @@ mod tests {
     use super::*;
     use image::ImageBuffer;
     use std::iter::FromIterator;
+
+    #[test]
+    fn test_sub_chunk_positions() {
+        let side = 2;
+        let basic_matrix = DMatrix::from_column_slice(
+            side,
+            side,
+            &vec![1, 2, 3, 4]
+        );
+        let expected = vec![
+            (0, 0, 1, 1),
+            (0, 1, 1, 1),
+            (1, 0, 1, 1),
+            (1, 1, 1, 1),
+            (0, 0, 2, 1),
+            (0, 1, 2, 1),
+            (0, 0, 1, 2),
+            (1, 0, 1, 2),
+            (0, 0, 2, 2)
+        ];
+
+        let result = sub_chunk_positions(&basic_matrix, side as u32);
+
+        //assert_eq!()
+    }
 
     #[test]
     fn test_alias_pixels() {
@@ -223,5 +385,65 @@ mod tests {
         let rotation_matrix = DMatrix::from_row_slice(2, 2, &vec![0, -1, 1, 0]);
         let rotated_matrix = basic_matrix * rotation_matrix;
         //println!("{:?}", rotated_matrix);
+    }
+
+    #[test]
+    fn what_sort_of_matrix_is_this() {
+        let v = vec![0, 1, 2, 3];
+        let matrix = DMatrix::from_row_slice(2, 2, &v);
+        println!("{}", matrix);
+        //                     y, x
+        //                    (column position, row position)
+        println!("{}", matrix[(1, 0)]);
+    }
+
+    // how do you test that this works?
+    #[test]
+    fn test_set_to_map() {
+        let set = HashSet::from_iter(vec![10, 11, 12, 13]);
+
+        let map = set_to_map(set);
+        let values: Vec<_> = map.values().collect();
+
+        // confirm that hashset values were correctly submitted to map›
+        assert!(values.iter().any(|&x| x == &10));
+        assert!(values.iter().any(|&x| x == &11));
+        assert!(values.iter().any(|&x| x == &11));
+        assert!(values.iter().any(|&x| x == &11));
+    }
+
+    #[test]
+    fn d_matrix_columns() {
+        let v = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let matrix = DMatrix::from_row_slice(3, 3, &v);
+        println!("{}", matrix);
+        let x = matrix.ncols();
+        println!("{:?}", matrix.remove_columns(10, 0));
+        // println!("{}", matrix.column_part(2, 2));
+        // println!("{}", matrix.row_part(2, 3).remove_columns(0, 2));
+        // println!("{}", matrix.clone().remove_columns(0, 1).remove_rows(0, 1));
+        // println!("{}", matrix.clone().remove_columns(1, 2).remove_rows(2, 1));
+        // println!("{}", matrix.clone().remove_columns(0, 1).remove_columns(2, 1).remove_rows(0, 1).remove_rows(2, 1));
+        // println!("{}", matrix.remove_columns(0, 0).remove_columns(2, 1).remove_rows(0, 1).remove_rows(1, 1));
+        // (0..2).for_each(|| {
+        // });
+    }
+
+    #[test]
+    fn test_sub_matrix() {
+        let v = vec![0, 1, 2, 3, 4, 5, 6, 7, 8];
+        let matrix = DMatrix::from_row_slice(3, 3, &v);
+        //
+        // let x = vec![4, 5];
+        // let target_a = DMatrix::from_row_slice(1, 2, &x);
+        //
+        // let x = vec![1, 2, 4, 5];
+        // let target_b = DMatrix::from_row_slice(2, 2, &x);
+
+        // assert_eq!(matrix.sub_matrix((1, 1), (2, 1)), target_a);
+
+        println!("{}", matrix.sub_matrix((0, 1), (1, 1)))
+
+        // assert_eq!(matrix.sub_matrix((1, 0), (2, 2)), target_b);
     }
 }
