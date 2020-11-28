625a47f0953b80e9e15c552414c0faa891734aaa
Dec Kolakowski <declankolakowski@gmail.com>
2020-11-28 14:22:25 +0000

    Create limit iter

---
+++ src/io/limit_iter.rs
@@ -0,0 +1,28 @@
+struct Limit {
+    curr: i32,
+    base: i32
+}
+
+impl Iterator for Limit {
+    type Item = u32;
+    fn next(&mut self) -> Option<u32> {
+        self.curr += 1;
+        if self.curr < (self.base - 1) { return Some(0) }
+        Some((self.curr - (self.base - 1)) as u32)
+    }
+}
+
+fn limit_iter(base: u32) -> Limit {
+    Limit { base: (base as i32), curr: -1 }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_limit_iter() {
+        let wave = limit_iter(3).take(10).collect::<Vec<u32>>();
+        assert_eq!(wave, vec![0, 0, 0, 1, 2, 3, 4, 5, 6, 7]);
+    }
+}
--- src/io/tri_wave.rs
+++ src/io/tri_wave.rs
@@ -1,11 +1,11 @@
 use std::f32::consts::PI;
 
-struct tri_wave {
+struct ITriWave {
     period: f32,
     curr: i32
 }
 
-impl Iterator for tri_wave {
+impl Iterator for ITriWave {
     type Item = u32;
     fn next(&mut self) -> Option<u32> {
         let amplitude = self.period / 4.0;
@@ -20,12 +20,31 @@ impl Iterator for tri_wave {
     }
 }
 
-fn tri_wave(base: u32) -> tri_wave {
-    tri_wave { period: ((base * 2) -2) as f32, curr: 0 }
+// Creates a integer triangle wave iterator that can return values for ALL values of x
+fn i_tri_wave(base: u32, start: i32) -> ITriWave {
+    ITriWave { period: ((base * 2) -2) as f32, curr: start }
 }
 
-fn i_tri_wave(base: u32, start: i32) -> tri_wave {
-    tri_wave { period: ((base * 2) -2) as f32, curr: start }
+struct UTriWave {
+    base: i32,
+    period: i32,
+    curr: i32
+}
+
+impl Iterator for UTriWave {
+    type Item = u32;
+    fn next(&mut self) -> Option<u32> {
+        let position = self.curr % self.period;
+        self.curr+=1;
+        if position < (self.base - 1) { return Some(position as u32) }
+        Some((-position + self.period) as u32)
+    }
+}
+
+// Creates a integer triangle wave iterator that can calculates values for positive values of x
+fn u_tri_wave(base: u32) -> UTriWave {
+    let period = ((base * 2) -2) as i32;
+    UTriWave { base: (base as i32), period, curr: 0 }
 }
 
 #[cfg(test)]
@@ -33,12 +52,15 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_tri_wave() {
-        tri_wave(4).take(20).for_each(|x| println!("{}", x));
+    fn test_i_tri_wave() {
+        let wave = i_tri_wave(3, -5).take(10).collect::<Vec<u32>>();
+        // not 100% sure this is returning the correct values based on the graph
+        assert_eq!(wave, vec![2, 1, 0, 1, 2, 1, 0, 1, 2, 1]);
     }
 
     #[test]
-    fn test_i_tri_wave() {
-        i_tri_wave(4, -10).take(20).for_each(|x| println!("{}", x));
+    fn test_u_tri_wave() {
+        let wave = u_tri_wave(3).take(10).collect::<Vec<u32>>();
+        assert_eq!(wave, vec![0, 1, 2, 1, 0, 1, 2, 1, 0, 1]);
     }
 }
