00904415c192b71fb9e1872c466b73b90f865833
Dec Kolakowski <declankolakowski@gmail.com>
2021-04-24 15:13:33 +0100

    Attempt to implement Add for rgb pixels

--- src/io/misc_utils/rgb_utils.rs
+++ src/io/misc_utils/rgb_utils.rs
@@ -1,4 +1,5 @@
 use image::{Rgb, Pixel, Primitive};
+use std::ops::Add;
 
 pub trait PixelOperations<D>
 where
@@ -17,6 +18,24 @@ where
             self.channels()[0] + D::from(x.channels()[0]).unwrap(),
             self.channels()[1] + D::from(x.channels()[1]).unwrap(),
             self.channels()[2] + D::from(x.channels()[2]).unwrap()
+        ])
+    }
+}
+
+impl<D> std::ops::Add for Rgb<D>
+where
+    D: Primitive
+{
+    type Output = Rgb<D>;
+
+    fn add<T>(self, rhs: Rgb<T>) -> Self::Output
+    where
+        T: Primitive
+    {
+        Rgb::from([
+            self.channels()[0] + D::from(rhs.channels()[0]).unwrap(),
+            self.channels()[1] + D::from(rhs.channels()[1]).unwrap(),
+            self.channels()[2] + D::from(rhs.channels()[2]).unwrap()
         ])
     }
 }
